22ba1b5652a2f194dcc0ec58407a4cf82a4ee8b5
plumsirawit <plurm2545@hotmail.com>
2020-05-19 02:11:02 +0700

    implement latestPosts

---
+++ functions/posts/latestPosts.js
@@ -0,0 +1,25 @@
+const fb = require("firebase-admin")
+const db = fb.firestore()
+module.exports = async (req, res) => {
+    try {
+        var user = await fb.auth().getUser(req.authId)
+        user = user.toJSON()
+        var query = await db
+            .collection("posts")
+            .orderBy("d.createdAt", "desc")
+            .get()
+        var data = []
+        query.forEach(doc => {
+            var temp = doc.data().d
+            temp.createdAt = temp.createdAt.toDate()
+            data.push({
+                data: temp,
+                id: doc.id,
+            })
+        })
+        return res.send(data)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send("error")
+    }
+}
