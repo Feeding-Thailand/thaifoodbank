b281da584eb51b408d242ed9680d88b666f8d983
plumsirawit <plurm2545@hotmail.com>
2020-05-19 14:52:01 +0700

    add recaptcha verification

---
+++ functions/checkRecaptcha.js
@@ -0,0 +1,27 @@
+const axios = require("axios")
+const secret = require("./recaptchaSecret.js")
+
+const recaptchaVerifyURL = "https://www.google.com/recaptcha/api/siteverify"
+
+const checkRecaptcha = async (req, res, next) => {
+    try {
+        const { userResponse, userRemoteip } = req.query
+        const response = await axios.post(recaptchaVerifyURL, {
+            secret,
+            response: userResponse,
+            remoteip: userRemoteip,
+        })
+        if (response.success) {
+            return next()
+        } else {
+            return res.status(400).send("Unsuccessful reCAPTCHA verification")
+        }
+    } catch (e) {
+        console.log(e)
+        return res.status(500).send({
+            error: "Unexpected API Error",
+        })
+    }
+}
+
+module.exports = checkRecaptcha
