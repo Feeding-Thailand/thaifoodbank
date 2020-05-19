b469346c12b7c286c59dbbc3cea902901d5b1d7e
plumsirawit <plurm2545@hotmail.com>
2020-05-19 15:32:15 +0700

    [fix] recaptcha API mechanism

--- functions/checkRecaptcha.js
+++ functions/checkRecaptcha.js
@@ -5,14 +5,17 @@ const recaptchaVerifyURL = "https://www.google.com/recaptcha/api/siteverify"
 
 const checkRecaptcha = async (req, res, next) => {
     try {
-        const { response: userResponse } = req.query
-        const response = await axios.post(recaptchaVerifyURL, {
+        if (!req.query.response) {
+            return res.status(400).send("Response not found")
+        }
+        const response = await axios.post(recaptchaVerifyURL, null, {params: {
             secret,
-            response: userResponse,
-        })
-        if (response.success) {
+            response: req.query.response,
+        }})
+        if (response.data.success) {
             return next()
         } else {
+            console.log(response.data)
             return res.status(400).send("Unsuccessful reCAPTCHA verification")
         }
     } catch (e) {
