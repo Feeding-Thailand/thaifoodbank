8dae58e650cfb24a7780152339db995e4dfbbcd3
plumsirawit <plurm2545@hotmail.com>
2020-05-20 11:52:14 +0700

    update contactPost to return status

--- functions/post/contactPost.js
+++ functions/post/contactPost.js
@@ -8,8 +8,9 @@ module.exports = async (req, res) => {
             return
         }
         var snap = await db.collection("posts").doc(id).get()
-        const { contact } = snap.data().d
-        res.send({ contact })
+        const { contact, donors } = snap.data().d
+        const isAlreadyDonated = donors.findIndex(donor => donor.uid === req.authId) !== -1
+        res.send({ contact, isAlreadyDonated })
     } catch (err) {
         console.log(err)
         res.status(500).send("error")
