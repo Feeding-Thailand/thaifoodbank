9acfec687ff4845760c9d11e894e8067bc6e515a
plumsirawit <plurm2545@hotmail.com>
2020-05-21 03:14:42 +0700

    [attempt] pubsub scheduler

---
+++ functions/doDelete.js
@@ -0,0 +1,16 @@
+const firebase_tools = require("firebase-tools")
+const firebaseToken = require("./firebaseToken")
+module.exports = async (id) => {
+    await firebase_tools.firestore.delete(`posts/${id}`, {
+        project: process.env.GCLOUD_PROJECT,
+        recursive: true,
+        yes: true,
+        token: firebaseToken,
+    })
+    db.collection("stats")
+        .doc("stats")
+        .update({
+            currentPosts: fb.firestore.FieldValue.increment(-1),
+            deletedPosts: fb.firestore.FieldValue.increment(1),
+        })
+}
--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,17 @@
-const firebase = require("firebase-admin");
+const firebase = require("firebase-admin")
 firebase.initializeApp({
-    storageBucket: "thaifoodbank.appspot.com"
-});
+    storageBucket: "thaifoodbank.appspot.com",
+})
 const functions = require("firebase-functions")
 const post = require("./post")
 const posts = require("./posts")
 const donate = require("./donate")
+const cleaner = require("./cleaner")
 exports.post = functions.region("asia-east2").https.onRequest(post)
 exports.posts = functions.region("asia-east2").https.onRequest(posts)
 exports.donate = functions.region("asia-east2").https.onRequest(donate)
+exports.cleaner = functions
+    .region("asia-east2")
+    .pubsub.schedule("0 0 * * *")
+    .timeZone("Asia/Bangkok")
+    .onRun(cleaner)
