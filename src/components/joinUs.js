1df40cbe98ddba00003d1bfdbd27b8795a584fc9
Porames Vatanaprasan <s5907258@mwit.ac.th>
2020-05-21 17:20:53 +0700

    finaling finalize

--- src/components/joinUs.js
+++ src/components/joinUs.js
@@ -5,8 +5,8 @@ const JoinUs = () => (
 		<div className='container text-center'>
 			<h2 className='text-white'>ร่วมเป็นส่วนหนึ่งกับพวกเรา</h2>
 			<div className='mt-3'>
-				<button style={{ width: 100 }} className="btn mr-4 btn-light">ร่วมบริจาค</button>
-				<button style={{ width: 100 }} className="btn btn-outline-light">ติดต่อเรา</button>
+				<button disabled style={{ width: 100 }} className="btn mr-4 btn-light">ร่วมบริจาค</button>
+				<a target='_blank' href='https://facebook.com/feedingthailand' style={{ width: 100 }} className="btn btn-outline-light">ติดต่อเรา</a>
 			</div>
 		</div>
 	</div>
