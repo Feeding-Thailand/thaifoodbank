ec6582c53a27ac1ee1a7a43b7b60f8672b0266f8
plumsirawit <plurm2545@hotmail.com>
2020-05-21 22:25:11 +0700

    [hotfix] get document

--- functions/posts/oldestPosts.js
+++ functions/posts/oldestPosts.js
@@ -8,7 +8,7 @@ module.exports = async (req, res) => {
             .orderBy("d.createdAt", "asc")
             .where("d.active", "==", true)
         if (lastdocId) {
-            const docSnap = await db.collection("posts").doc(lastdocId)
+            const docSnap = await db.collection("posts").doc(lastdocId).get()
             fpart = fpart.startAfter(docSnap)
         }
         const query = await fpart.limit(12).get()
