75c15fb4218b099010914f24854cebbe6f8c16a3
Porames Vatanaprasan <s5907258@mwit.ac.th>
2020-05-22 15:17:05 +0700

    minor details

--- src/pages/help-matcher/map.js
+++ src/pages/help-matcher/map.js
@@ -6,13 +6,18 @@ import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import axios from 'axios'
 import { apiEndpoint } from '../../components/constants'
+import _ from 'lodash'
+import Person from '../../components/personCard'
+import { Link } from 'gatsby'
+import Spinner from 'react-bootstrap/Spinner'
 
 export default class Mapping extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             requestGeo: true,
-            position: {}
+            position: {},
+            data: 'loading'
         }
     }
     async getGeo() {
@@ -22,7 +27,7 @@ export default class Mapping extends React.Component {
                 console.log(geo)
                 this.setState({ position: geo, requestGeo: false })
                 const req = await axios.get(`${apiEndpoint}/posts/${geo.lat}/${geo.lng}/1000`)
-                console.log(req.data)
+                this.setState({ data: req.data }, () => console.log(this.state.data))
             })
         }
         else {
@@ -52,19 +57,40 @@ export default class Mapping extends React.Component {
                             </p>
                         </Modal.Body>
                         <Modal.Footer>
-                            <Button onClick={()=>window.location.replace('/help-matcher')} variant='light'>ไม่ยินยอม</Button>
+                            <Button onClick={() => window.location.replace('/help-matcher')} variant='light'>ไม่ยินยอม</Button>
                             <Button onClick={async () => await this.getGeo()}>ยินยอม</Button>
                         </Modal.Footer>
                     </Modal>
 
                     <div className='container pt-5 pb-5'>
                         <div className='row mb-4'>
-                            <div className='col-12'>
+                            <div className='col-6'>
                                 <h2>ผู้ต้องการความช่วยเหลือรอบตัวท่าน</h2>
+                            </div>
+                            <div className='col-md-6 pb-3 d-flex justify-content-md-end align-items-md-start'>
+                                <Link to='/help-matcher' className='btn btn-primary'>ดูผู้ต้องการความช่วยเหลือทั้งหมด</Link>
                             </div>
                         </div>
                         <div className='row'>
-
+                            {this.state.data !== 'loading' && !_.isEmpty(this.state.data) &&
+                                this.state.data.map((item, index) => {
+                                    return (
+                                        <Person data={{ ...item.data, id: item.id }} key={index} />
+                                    )
+                                })
+                            }
+                            {_.isEmpty(this.state.data) &&
+                                <div className='container'>
+                                    <div className='w-100 pt-5 pb-5 alert alert-dark'>
+                                        <h4 className='text-center text-muted mb-0'>ไม่พบข้อมูลผู้ต้องการความช่วยเหลือ</h4>
+                                    </div>
+                                </div>
+                            }
+                            {this.state.data === 'loading' &&
+                                <div className='flex-center pt-5 pb-5 w-100' style={{ alignItems: 'center' }}>
+                                    <Spinner variant='primary' animation="border" />
+                                </div>
+                            }
                         </div>
                     </div>
 
