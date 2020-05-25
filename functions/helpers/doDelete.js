21a699bdd226982a05363c37577df29030c941e4
plumsirawit <plurm2545@hotmail.com>
2020-05-25 21:15:05 +0700

    implement deleteDirectory to delete from storage

--- functions/helpers/doDelete.js
+++ functions/helpers/doDelete.js
@@ -2,6 +2,11 @@ const firebase_tools = require("firebase-tools")
 const firebaseToken = require("../secrets/firebaseToken")
 const fb = require("firebase-admin")
 const db = fb.firestore()
+const bucket = fb.storage().bucket()
+const deleteDirectory = async directory => {
+    const [files] = await bucket.getFiles({ prefix: "/" + directory })
+    await Promise.all(files.map(file => file.delete()))
+}
 module.exports = async id => {
     await Promise.all([
         firebase_tools.firestore.delete(`posts/${id}`, {
@@ -17,5 +22,6 @@ module.exports = async id => {
                 currentPosts: fb.firestore.FieldValue.increment(-1),
                 deletedPosts: fb.firestore.FieldValue.increment(1),
             }),
+        deleteDirectory(id)
     ])
 }
